e3a768c07a2ce87991219d7a21ae6cb4f126c439
GSvenny <gabrielswagner7@gmail.com>
2022-07-03 21:46:06 -0500

    added API

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const { animals } = require(`./data/animals`)
+
+app.get('/api/animals', (req, res) => {
+    let results = animals;
+    console.log(req.query)
+    res.json(animals);
+  });
 
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
