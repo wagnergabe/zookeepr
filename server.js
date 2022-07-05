5e14503f7b64327a8f33af1f55f484f2fb9f5ca6
GSvenny <gabrielswagner7@gmail.com>
2022-07-05 13:14:00 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -2,10 +2,41 @@ const express = require('express');
 const app = express();
 const { animals } = require(`./data/animals`)
 
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = [];
+    let filteredResults = animalsArray;
+
+    if(query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraitsArray = [query.personalityTraits];
+        } else {
+            personalityTraitsArray = query.personalityTraits;
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            );
+        });
+    }
+
+    if ( query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    }
+    if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    }
+    if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
-    console.log(req.query)
-    res.json(animals);
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
   });
 
 app.listen(3001, () => {
