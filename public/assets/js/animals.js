db7bbe399591ba52da600cc42025d7f14ef6d1b6
GSvenny <gabrielswagner7@gmail.com>
2022-07-07 15:41:22 -0500

    updated HTML/javascript

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,3 +1,5 @@
+const { response } = require("express");
+
 const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
@@ -30,6 +32,18 @@ const getAnimals = (formData = {}) => {
   });
 
   console.log(queryUrl);
+
+  fetch(queryUrl)
+    .then(response => {
+      if (!response.ok) {
+        return alert('Error: ' + response.statusText);
+      }
+      return response.json();
+    })
+    .then(animalData => {
+      console.log(animalData);
+      printResults(animalData);
+    })
 
 };
 
